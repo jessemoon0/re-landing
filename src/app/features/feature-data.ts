6cf2b3c3f488cfa23da17bbc344e6237fe64d298
Jessie J Valladares <shajessie@me.com>
2018-04-05 09:14:36 -0400

    Features and reviews section complete

--- src/app/features/feature-data.ts
+++ src/app/features/feature-data.ts
@@ -3,9 +3,37 @@ import {IFeature} from './IFeature';
 
 export const FEATURES_DATA: Array<IFeature> = [
   {
-    svgIcon: '../../../assets/img/sprite.svg#icon-global',
+    svgIcon: 'global',
     title: 'World\'s best luxury homes',
     text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. ' +
     'Tenetur distinctio necessitatibus pariatur voluptatibus.'
+  },
+  {
+    svgIcon: 'trophy',
+    title: 'Only the best properties',
+    text: 'Voluptatum mollitia quae. ' +
+    'Vero ipsum sapiente molestias accusamus rerum sed a eligendi aut quia.'
+  },
+  {
+    svgIcon: 'map-pin',
+    title: 'All homes in top locations',
+    text: 'Tenetur distinctio necessitatibus pariatur voluptatibus quidem consequatur harum.'
+  },
+  {
+    svgIcon: 'key',
+    title: 'New home in one month',
+    text: 'Vero ipsum sapiente molestias accusamus rerum. ' +
+    'Lorem, ipsum dolor sit amet consectetur adipisicing elit.'
+  },
+  {
+    svgIcon: 'presentation',
+    title: 'Top 1% realtors',
+    text: 'Quidem consequatur harum, voluptatum mollitia quae. ' +
+    'Tenetur distinctio necessitatibus pariatur voluptatibus.'
+  },
+  {
+    svgIcon: 'lock',
+    title: 'Secure payments on Nexter',
+    text: 'Pariatur voluptatibus quidem consequatur harum, voluptatum mollitia quae.'
   }
 ];
